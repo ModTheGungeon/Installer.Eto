d1b58ec29153edefcb314f233ebba91c7ad07d7d
Zatherz <zatherz@linux.pl>
2018-02-03 11:55:11 +0100

    Travis 3

--- create_distrib.sh
+++ create_distrib.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir -p distrib/bin
-msbuild /p:Configuration=Release
+msbuild /p:Configuration=Release /p:Platform=x86
 cp -r MTGInstallerEto/bin/Release/* distrib/bin
 cd distrib
 zip -r MTGInstaller.zip *
